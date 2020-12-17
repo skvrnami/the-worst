2c809ec64418ed8c909b22ededa0113498413fcc
Michael Škvrňák <397925@mail.muni.cz>
2020-12-17 22:37:52 +0100

    Order songs in playlist by clustering

--- 02_best_new_music.R
+++ 02_best_new_music.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(RSQLite)
+library(spotifyr)
 
 STATION_ID <- "radiowave"
 
@@ -14,10 +15,13 @@ playlist <- dbGetQuery(con, "select * from playlist") %>%
            week_no = lubridate::week(ymd), 
            last_week = week_no == max(week_no)) 
 
+MAX_WEEK <- max(playlist$week_no)
+
 spotify_tracks <- dbGetQuery(con, 
                              "select 
                                 interpret_id, 
                                 track_id, 
+                                spotify_track_id,
                                 spotify_url, 
                                 track_name 
                              from spotify_tracks")
@@ -36,19 +40,54 @@ new_music <- playlist %>%
 
 nrow(new_music)
 
+last_week <- playlist %>%
+    filter(last_week) %>%
+    filter(station_id == STATION_ID) %>%
+    count(interpret_id, track_id, sort = TRUE) %>% 
+    left_join(., interprets, by = "interpret_id") %>% 
+    left_join(., spotify_tracks, by = c("interpret_id", "track_id")) %>%
+    # filter(!is.na(spotify_url)) %>%
+    mutate(song = paste0(interpret, ": ", track_name)) %>%
+    select(song, spotify_url, spotify_track_id, n) %>%
+    head(., 30)
+
 best_new_music <- new_music %>%
     left_join(., interprets, by = "interpret_id") %>% 
     left_join(., spotify_tracks, by = c("interpret_id", "track_id")) %>%
-    filter(!is.na(spotify_url)) %>%
+    # filter(!is.na(spotify_url)) %>%
     mutate(song = paste0(interpret, ": ", track_name)) %>%
-    select(song, spotify_url, n) %>%
+    select(song, spotify_url, spotify_track_id, n) %>%
     head(., 30)
 
 MD_TITLE <- glue::glue("# New {STATION_ID} music")
 MD_BODY <- purrr::map2_chr(best_new_music$song, best_new_music$spotify_url, 
                            function(song, url) {
+                               if(is.na(url)){
+                                   url <- glue::glue("https://www.youtube.com/results?search_query={song}")
+                               }
                                glue::glue("- [{song}]({url})\n")
                            })
 
-writeLines(c(MD_TITLE, "\n", MD_BODY), con = paste0("output/", STATION_ID, ".md"))
+writeLines(c(MD_TITLE, "\n", MD_BODY), con = paste0("output/", STATION_ID, "_", MAX_WEEK, ".md"))
+
+# order playlist using hierarchical clustering by audio features
+
+SPOTIFY_IDS <- best_new_music %>%
+    filter(!is.na(spotify_track_id)) %>%
+    pull(spotify_track_id)
+
+best_nm_features <- get_track_audio_features(SPOTIFY_IDS)
+
+hc <- best_nm_features %>%
+    select(danceability, energy, key, loudness, 
+           mode, speechiness, acousticness, instrumentalness, 
+           liveness, valence, tempo) %>%
+    dist %>%
+    hclust
+
+best_new_music_clustered <- best_new_music %>%
+    filter(!is.na(spotify_track_id)) %>%
+    slice(hc$order)
+
+# TODO: create playlist on Spotify
     
