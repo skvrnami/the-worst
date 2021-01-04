35f640f330bd029509bf355f59170ea7df7df7aa
Michael Škvrňák <397925@mail.muni.cz>
2021-01-04 18:56:16 +0100

    Use isoweek and isoyear

--- 02_best_new_music.R
+++ 02_best_new_music.R
@@ -21,15 +21,23 @@ convert_to_date <- function(x){
 playlist <- readRDS("output/playlist.RData") %>%
     filter(station_id == STATION_ID) %>%
     mutate(ymd = convert_to_date(date), 
-           week_no = lubridate::week(ymd), 
-           last_week = week_no == max(week_no)) 
+           week_no = lubridate::isoweek(ymd), 
+           year = lubridate::isoyear(ymd)) 
 
-MAX_WEEK <- max(playlist$week_no)
-MAX_YEAR <- playlist %>% 
-    filter(last_week) %>%
+MAX_WEEK <- playlist %>% 
+    filter(year == max(year)) %>%
     pull(date) %>%
-    min %>%
-    lubridate::year()
+    max %>%
+    lubridate::isoweek()
+
+MAX_YEAR <- playlist %>% 
+    filter(year == max(year)) %>%
+    pull(date) %>%
+    max %>%
+    lubridate::isoyear()
+
+playlist <- playlist %>%
+    mutate(last_week = week_no == MAX_WEEK & year == MAX_YEAR)
 
 spotify_tracks <- readRDS("output/spotify_tracks.RData") %>%
     select(interpret_id, 
@@ -75,7 +83,7 @@ best_new_music <- new_music %>%
     unique %>% 
     select(song, spotify_url, spotify_track_id, n)
 
-write_md <- function(df, station_id, max_week){
+write_md <- function(df, station_id, max_week, max_year){
     MD_TITLE <- glue::glue("# Discover {station_id} weekly")
     MD_BODY <- purrr::map2_chr(df$song, df$spotify_url, 
                                function(song, url) {
@@ -85,13 +93,13 @@ write_md <- function(df, station_id, max_week){
                                    glue::glue("- [{song}]({url})\n")
                                })
     
-    writeLines(c(MD_TITLE, "\n", MD_BODY), con = glue::glue("output/md/{station_id}_{max_week}.md"))
+    writeLines(c(MD_TITLE, "\n", MD_BODY), con = glue::glue("output/md/{station_id}_{max_week}_{max_year}.md"))
     
 }
 
 best_new_music %>%
     head(30) %>%
-    write_md(., STATION_ID, MAX_WEEK)
+    write_md(., STATION_ID, MAX_WEEK, MAX_YEAR)
 
 # order playlist using hierarchical clustering by audio features
 best_new_music_on_spotify <- best_new_music %>%
@@ -121,7 +129,7 @@ write.csv(bnm_final, glue::glue("output/csv/discover_{STATION_ID}_{MAX_YEAR}_(#{
           row.names = FALSE)
 
 # TODO: create playlist on Spotify
-# spotify_playlist <- create_playlist("majky_", glue::glue("Discover {STATION_NAME} Weekly (#{MAX_WEEK})"),
+# spotify_playlist <- create_playlist("majky_", glue::glue("Discover {STATION_NAME} Weekly (#{MAX_WEEK}/{MAX_YEAR})"),
 #                                     public = FALSE)
 # add_tracks_to_playlist(spotify_playlist$id,
 #                        bnm_final$spotify_uri)
